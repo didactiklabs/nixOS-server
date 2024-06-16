c53a4cffc39ca3551c298d1b7cdb1c654f4fde23
Victor Hang <vhvictorhang@gmail.com>
2024-06-16 16:07:05 +0200

    feat: k3s module

--- profiles/dummy.nix
+++ profiles/dummy.nix
@@ -3,41 +3,23 @@
   pkgs,
   lib,
   username,
+  hostname,
   ...
 }: {
   customNixOSModules = {
-    laptopProfile.enable = true;
-    networkManager.enable = true;
+    k3s.enable = false;
   };
-  networking.hostName = lib.mkForce "dummy-profile";
   home-manager = {
     users."${username}" = {
+      home.packages = [
+        pkgs.cilium-cli
+      ];
       customHomeManagerModules = {
-        bluetooth.enable = true;
-        fontConfig.enable = true;
         gitConfig.enable = true;
-        gtkConfig.enable = true;
-        sway.enable = true;
-        hyprland.enable = false;
         sshConfig.enable = true;
         starship.enable = true;
         vim.enable = true;
-        stylixConfig.enable = false;
-        # https://github.com/adi1090x/rofi
-        rofiConfig = {
-          enable = true;
-          launcher = {
-            type = "type-1";
-            style = "style-1";
-          };
-          powermenu = {
-            style = "style-1"; #(1 - 5) # type-1 only
-          };
-          color = "onedark";
-          copyqConfig.enable = true;
-          fastfetchConfig.enable = true;
-          desktopApps.enable = true;
-        };
+        fastfetchConfig.enable = true;
       };
     };
   };
--- profiles/khoa-nixhome/default.nix
+++ profiles/khoa-nixhome/default.nix
@@ -6,21 +6,14 @@
   hostname,
   ...
 }: {
-  networking.firewall.allowedTCPPorts = [
-    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
-    # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
-    # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
-  ];
-  networking.firewall.allowedUDPPorts = [
-    # 8472 # k3s, flannel: required if using multi-node for inter-node networking
-  ];
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
-    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
-  ];
+  customNixOSModules = {
+    k3s.enable = true;
+  };
   home-manager = {
     users."${username}" = {
+      home.packages = [
+        pkgs.cilium-cli
+      ];
       customHomeManagerModules = {
         gitConfig.enable = true;
         sshConfig.enable = true;
