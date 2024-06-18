7afd1fdcc0f3e02d9d5d7493f3238d7dd1ac0b09
Victor Hang <vhvictorhang@gmail.com>
2024-06-18 19:18:00 +0200

    feat: systemd service to keep up to date

--- nixosModules/scripts.nix
+++ nixosModules/scripts.nix
@@ -9,11 +9,35 @@
   novaLauncher =
     pkgs.writeShellScriptBin "novaLauncher"
     ''
-      novaInstall --username ${username} --hostname ${hostname} --repo https://${nixos_gitrepo} --branch main
+      date >> /var/log/nova.log
+      echo "-----" >> /var/log/nova.log
+      echo "-----" >> /var/log/nova.log
+      novaInstall --username ${username} --hostname ${hostname} --repo https://${nixos_gitrepo} --branch main >> /var/log/nova.log
     '';
 in {
   environment.systemPackages = [
     novaInstall
     novaLauncher
   ];
+  systemd.services.novalauncher = {
+    description = "NovaLauncher script to maintain the server up to date with git.";
+    partOf = ["default.target"];
+    requires = ["default.target"];
+    after = ["default.target"];
+    wantedBy = ["default.target"];
+    serviceConfig = {
+      User = "root";
+      Group = "root";
+      Type = "oneshot";
+      ExecStart = "${novaLauncher}/bin/novaLauncher";
+    };
+  };
+  systemd.timers."novalauncher" = {
+    wantedBy = ["timers.target"];
+    timerConfig = {
+      OnBootSec = "5m";
+      OnUnitActiveSec = "1m";
+      Unit = "novalauncher.service";
+    };
+  };
 }
