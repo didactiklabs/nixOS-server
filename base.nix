e4e94eee770e03fa584748a4b587eb563945a8be
Victor Hang <vhvictorhang@gmail.com>
2024-07-05 12:25:45 +0200

    fix: typos

--- base.nix
+++ base.nix
@@ -1,7 +1,6 @@
 {
-  pkgs ?
-    import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/706eef542dec88cc0ed25b9075d3037564b2d164.tar.gz") {}
-    config,
+  pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/706eef542dec88cc0ed25b9075d3037564b2d164.tar.gz") {},
+  config,
   hostname,
   lib,
   nixos_gitrepo ? "https://github.com/didactiklabs/nixOS-server.git",
