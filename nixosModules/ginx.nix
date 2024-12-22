9171e2add0c67124bfb483fb945d38749db5fc46
Victor Hang <vhvictorhang@gmail.com>
2024-12-22 20:16:15 +0100

    fix 🐛: ginx env for colmena-- attempt
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- nixosModules/ginx.nix
+++ nixosModules/ginx.nix
@@ -26,14 +26,9 @@ in
         path = [
           pkgs.colmena
         ];
-        environment = {
-          NIX_PROFILES = "/run/current-system/sw /nix/var/nix/profiles/default /etc/profiles/per-user/root /root/.local/state/nix/profile /nix/profile /root/.nix-profile";
-          NIX_PATH = "/root/.nix-defexpr/channels:nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos:nixos-config=/etc/nixos/configuration.nix:/nix/var/nix/profiles/per-user/root/channels";
-          NIXPKGS_CONFIG = "/etc/nix/nixpkgs-config.nix";
-        };
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
-          ExecStart = "${pkgs.bash}/bin/bash -c '${ginx}/bin/ginx --source https://github.com/didactiklabs/nixOs-server -b main -n 60 -- colmena apply-local --sudo'";
+          ExecStart = "${pkgs.bash}/bin/bash -c '${ginx}/bin/ginx --source https://github.com/didactiklabs/nixOs-server -b main -n 60 -- colmena apply-local'";
           Restart = "on-failure";
         };
       };
