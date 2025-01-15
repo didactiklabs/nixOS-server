ce96390186ab4e5db9feefb4a82209ad4e052094
Victor Hang <vhvictorhang@gmail.com>
2025-01-15 21:07:06 +0100

    feat ✨: add a svc to correct drift with source
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- nixosModules/ginx.nix
+++ nixosModules/ginx.nix
@@ -16,6 +16,13 @@ let
     echo Running ginx...
     ${ginx}/bin/ginx --source https://github.com/didactiklabs/nixOs-server -b main --now -- ${pkgs.colmena}/bin/colmena apply-local --sudo
   '';
+  ossync = pkgs.writeShellScriptBin "osupdate" ''
+    set -euo pipefail
+    if  [ "$(${pkgs.jq}/bin/jq .rev /etc/nixos/version)" != "$(${pkgs.git}/bin/git ls-remote https://github.com/didactiklabs/nixOs-server HEAD | awk '{print $1}')" ]; then
+      ${osupdate}/bin/osupdate
+    fi
+    OS is up to date.
+  '';
 in
 {
   options.customNixOSModules.ginx = {
@@ -35,25 +42,53 @@ in
     };
 
     systemd = {
-      services.ginx = {
-        enable = true;
-        path = [
-          pkgs.colmena
-        ];
-        wantedBy = [ "multi-user.target" ];
-        serviceConfig = {
-          ExecStart = "${pkgs.bash}/bin/bash -c '${ginx}/bin/ginx --source https://github.com/didactiklabs/nixOs-server -b main -n 60 --exit-on-fail -- colmena apply-local'";
-          Restart = "always";
+      services = {
+        ginx = {
+          enable = true;
+          path = [
+            pkgs.colmena
+          ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            ExecStart = "${pkgs.bash}/bin/bash -c '${ginx}/bin/ginx --source https://github.com/didactiklabs/nixOs-server -b main -n 60 --exit-on-fail -- colmena apply-local'";
+            Restart = "always";
+          };
+        };
+        ossync = {
+          enable = true;
+          path = [
+            pkgs.colmena
+            pkgs.jq
+            pkgs.git
+            pkgs.busybox
+          ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            ExecStart = "${pkgs.bash}/bin/bash -c '${ossync}/bin/ossync";
+            Restart = "always";
+          };
         };
       };
-      timers.ginx-timer = {
-        enable = true;
-        description = "Timer to run myService every 5 minutes";
-        wantedBy = [ "timers.target" ];
-        timerConfig = {
-          OnUnitActiveSec = "5min";
-          Persistent = true;
-          Unit = "ginx.service";
+      timers = {
+        ginx-timer = {
+          enable = true;
+          description = "Timer to run myService every 5 minutes";
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnUnitActiveSec = "5min";
+            Persistent = true;
+            Unit = "ginx.service";
+          };
+        };
+        ossync-timer = {
+          enable = true;
+          description = "Timer to run myService every 5 minutes";
+          wantedBy = [ "timers.target" ];
+          timerConfig = {
+            OnUnitActiveSec = "15min";
+            Persistent = true;
+            Unit = "ossync.service";
+          };
         };
       };
     };
