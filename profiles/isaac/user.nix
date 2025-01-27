5122d6d4eb3e2afb5c920ef7d0dc7215d686ae13
Victor Hang <vhvictorhang@gmail.com>
2025-01-27 22:13:24 +0100

    feat ✨: add runner dir
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

---
+++ profiles/isaac/user.nix
@@ -0,0 +1,22 @@
+{ ... }:
+{
+  home-manager.users."nixos".home = {
+    file = {
+      "runner1/.keep" = {
+        text = "";
+      };
+      "runner2/.keep" = {
+        text = "";
+      };
+      "runner3/.keep" = {
+        text = "";
+      };
+      "runner4/.keep" = {
+        text = "";
+      };
+      "runner5/.keep" = {
+        text = "";
+      };
+    };
+  };
+}
