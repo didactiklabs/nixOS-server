0f008b40b2aacf241815dacd573d6bcca189730a
Victor Hang <vhvictorhang@gmail.com>
2025-02-21 11:56:42 +0100

    chore 🧹(ossync): remove ossync
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- nixosModules/ginx.nix
+++ nixosModules/ginx.nix
@@ -16,15 +16,6 @@ let
     echo Running ginx...
     ${ginx}/bin/ginx --source https://github.com/didactiklabs/nixOs-server -b main --now -- ${pkgs.colmena}/bin/colmena apply-local --sudo
   '';
-  ossync = pkgs.writeShellScriptBin "ossync" ''
-    set -euo pipefail
-    if [ -e "/etc/nixos/version" ]; then
-      if  [ "$(${pkgs.jq}/bin/jq .rev /etc/nixos/version)" != "$(${pkgs.git}/bin/git ls-remote https://github.com/didactiklabs/nixOs-server HEAD | awk '{print $1}')" ]; then
-        ${osupdate}/bin/osupdate
-      fi
-      echo 'OS is up to date !'
-    fi
-  '';
 in
 {
   options.customNixOSModules.ginx = {
@@ -58,40 +49,16 @@ in
             Restart = "always";
           };
         };
-        ossync = {
-          enable = true;
-          path = [
-            pkgs.colmena
-            pkgs.jq
-            pkgs.git
-            pkgs.busybox
-          ];
-          wantedBy = [ "multi-user.target" ];
-          serviceConfig = {
-            ExecStart = "${pkgs.bash}/bin/bash -c '${ossync}/bin/ossync'";
-            Restart = "always";
-          };
-        };
-      };
-      timers = {
-        ginx-timer = {
-          enable = true;
-          description = "Timer to run myService every 5 minutes";
-          wantedBy = [ "timers.target" ];
-          timerConfig = {
-            OnUnitActiveSec = "5min";
-            Persistent = true;
-            Unit = "ginx.service";
-          };
-        };
-        ossync-timer = {
-          enable = true;
-          description = "Timer to run myService every 5 minutes";
-          wantedBy = [ "timers.target" ];
-          timerConfig = {
-            OnUnitActiveSec = "15min";
-            Persistent = true;
-            Unit = "ossync.service";
+        timers = {
+          ginx-timer = {
+            enable = true;
+            description = "Timer to run myService every 5 minutes";
+            wantedBy = [ "timers.target" ];
+            timerConfig = {
+              OnUnitActiveSec = "5min";
+              Persistent = true;
+              Unit = "ginx.service";
+            };
           };
         };
       };
