0de782f6b9156d4f55561b711c9328c850caba03
Victor Hang <vhvictorhang@gmail.com>
2025-10-05 00:34:17 +0200

    chore 🧹: update kube kaasopeia
    Signed-off-by: Victor Hang <vhvictorhang@gmail.com>

--- profiles/kaassopeia/default.nix
+++ profiles/kaassopeia/default.nix
@@ -105,8 +105,8 @@ in
     kubernetes = {
       enable = true;
       version = {
-        kubeadm = "v1.33.4";
-        kubelet = "v1.33.4";
+        kubeadm = "v1.34.0";
+        kubelet = "v1.34.0";
       };
     };
     caCertificates = {
